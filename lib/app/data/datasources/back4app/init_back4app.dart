298ee5627c72c2ad52e80082345ae153891b338e
catalunha <catalunha.mj@gmail.com>
2022-04-25 09:39:32 -0300

    init livequery

--- lib/app/data/datasources/back4app/init_back4app.dart
+++ lib/app/data/datasources/back4app/init_back4app.dart
@@ -9,10 +9,11 @@ class InitBack4app {
     const _keyApplicationId = 'x4uHx8hJECtAPiKo4Z2f8IK9OUKRtcuUBBaUBHM9';
     const _keyParseServerUrl = 'https://parseapi.back4app.com';
     const _keyClientKey = 'GoHjfXQ1fIClRQ6K9Gg9hc1dpKj90HJGRXcHeSrs';
+    const _keyLiveQueryUrl = 'https://contactlist01.b4a.io';
     await Parse().initialize(
-      _keyApplicationId,
-      _keyParseServerUrl,
+      _keyApplicationId, _keyParseServerUrl,
       clientKey: _keyClientKey,
+      liveQueryUrl: _keyLiveQueryUrl,
       autoSendSessionId: true,
       // debug: true,
     );
